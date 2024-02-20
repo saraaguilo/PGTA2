666b31b54a79f439fd642b4fc0127e61c54a65ec
Dsd234 <david.sola.diez@estudiantat.upc.edu>
2024-02-20 17:31:37 +0100

    Primera versió P2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\David\Desktop\UNI 2023-2024\PGTA\P2\230502-est-080001_BCN_60MN_08_09.ast";
+            Console.WriteLine("Hello World!");
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string linea;
+                // Lee y muestra cada línea del archivo
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                }
+            }
+        }
+    }
+}
